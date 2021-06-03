745bb4bb02409cc60d9aaadcfcacd05d231c3dda
Pablo <pgocev@mail.ru>
2021-06-03 15:49:44 +0300

    rest and default parameters

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //Cards
 
     class Card {
-        constructor(image, alt, header, description, price, parentSelector) {
+        constructor(image, alt, header, description, price, parentSelector, ...classes) {
             this.image = image;
             this.alt = alt;
             this.header = header;
@@ -150,6 +150,7 @@ window.addEventListener('DOMContentLoaded', () => {
             this.price = price;
             this.transfer = 27;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.changeToUAH();
         }
 
@@ -159,8 +160,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if(this.classes.length===0)
+            {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach((className)=>{
+                    element.classList.add(className);
+                });
+            }
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.image} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.header}</h3>
                     <div class="menu__item-descr">${this.description}</div>
@@ -169,7 +178,6 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
             `;
             document.querySelector('.menu .container').append(element);
         }
